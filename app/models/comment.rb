77d8624986467f6da0a7c0f5ef34cb34d4945cf9
massa <zoumaniguimassa12@gmail.com>
2023-07-20 21:54:16 +0000

    Fixed linter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
- belongs_to :author, class_name: 'User'
- belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
- after_save :update_post_comments_counter
- 
- def update_posts_comments_counter
-   post.increment!(:comments_counter)
- end
+  after_save :update_post_comments_counter
+
+  def update_posts_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
