ead135a2ec85c639c3e6bbe9624ae6199edfcf38
massa <zoumaniguimassa12@gmail.com>
2023-07-20 19:39:03 +0000

    Fixed linter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
- belongs_to :author, class_name: "User", foreign_key: "author_id"
- belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
- def self.update_post_likes_counter(post_id)
-   post = Post.find_by(id: post_id)
-   return unless post
+  def self.update_post_likes_counter(post_id)
+    post = Post.find_by(id: post_id)
+    return unless post
 
-   post.update(likes_counter: post.likes.count)
- end
+    post.update(likes_counter: post.likes.count)
+  end
 end
