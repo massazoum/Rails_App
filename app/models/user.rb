95f2d1d5d283ba1910533f9daa171dc4780d3f2a
massa <zoumaniguimassa12@gmail.com>
2023-07-22 04:38:47 +0000

    I added again test for update_posts_counter for user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-  # Method to retrieve the 3 most recent posts for a user
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
