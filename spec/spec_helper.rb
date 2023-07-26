2e867e82cfe7c1fcd5db98ca4ea090f7a090be03
massa <zoumaniguimassa12@gmail.com>
2023-07-26 00:05:52 +0000

    Tests added for controllers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 # specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
 # The generated `.rspec` file contains `--require spec_helper` which will cause
 # this file to always be loaded, without a need to explicitly require it in any
-# files.
+# files
 #
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
