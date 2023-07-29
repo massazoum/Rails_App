e46c076b16680d672212af6c1a8b1259874c8819
massa <zoumaniguimassa12@gmail.com>
2023-07-29 07:34:25 +0000

    Add forms

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,17 @@
+class LikesController < ApplicationController
+ def new
+  @post = Post.find(params[:post_id]) # Assuming you have a `post_id` parameter in the request
+  @like = Like.new
+end
+
+ def create
+   @post = Post.find(params[:post_id])
+   @like = @current_user.likes.build(post: @post)
+   if @like.save
+     redirect_to @post, notice: 'Liked the post!'
+   else
+     redirect_to @post, alert: 'Failed to like the post.'
+   end
+ end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
   root 'users#index'
+
   # Users routes
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: :user
@@ -8,4 +8,15 @@ Rails.application.routes.draw do
   # User posts routes
   get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
   get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
+
+# Route for handling the GET request to "/likes/new"
+# get '/likes/new', to: 'likes#new', as: :new_like
+
+  resources :posts do
+    resources :likes, only: [:create] # Nested route for creating likes for a post
+  end
+  # Create new post, comment, and like routes
+  # resources :posts, only: [:new, :create]
+  resources :comments, only: [:new, :create]
+  # resources :likes, only: [:create]
 end
