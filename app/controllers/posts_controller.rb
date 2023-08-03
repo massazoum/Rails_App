19bc7776c140e0beb687357fe545eb1bc654a9e9
massa <zoumaniguimassa12@gmail.com>
2023-08-03 17:40:29 +0000

    Fixing linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,3 @@
-# class PostsController < ApplicationController
-
-#   def index
-#     @user = User.find(params[:user_id])
-#     @posts = @user.posts
-#   end
-
-#   def show
-#     @user = User.find(params[:user_id])
-#     @post = @user.posts.find(params[:id])
-#   end
-
-#   def new
-#     @post = Post.new
-#   end
-
-#   def create
-#     @post = @current_user.posts.build(post_params)
-#     if @post.save
-#       redirect_to @post, notice: 'Post was successfully created.'
-#     else
-#       render :new
-#     end
-#   end
-# end
-
-
-
-
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
@@ -38,6 +9,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.includes(:comments).find(params[:id]) # Eager loading comments
   end
+  
   def new
     @post = Post.new
   end
