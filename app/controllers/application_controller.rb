4955106167640c7580d4264c500eb594af616b13
massa <zoumaniguimassa12@gmail.com>
2023-08-04 17:49:09 +0000

    Devise added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
+# class ApplicationController < ActionController::Base
+#   before_action :set_current_user
+
+#   private
+
+#   def set_current_user
+#     @current_user = User.first # Replace this with proper authentication logic in future projects
+#   end
+# end
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  protect_from_forgery with: :exception
 
-  private
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-  def set_current_user
-    @current_user = User.first # Replace this with proper authentication logic in future projects
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
   end
 end
