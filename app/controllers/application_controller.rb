e9e5824b6332f1a82a002cba2e88908a31bef36e
massa <zoumaniguimassa12@gmail.com>
2023-08-04 19:35:54 +0000

    Fixing :confirmation migration to database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
-# class ApplicationController < ActionController::Base
-#   before_action :set_current_user
-
-#   private
-
-#   def set_current_user
-#     @current_user = User.first # Replace this with proper authentication logic in future projects
-#   end
-# end
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
@@ -15,7 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
   end
 end
